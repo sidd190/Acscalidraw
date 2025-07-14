33673b80d2a7bb8a82967d231c8bb58b15034bb3
Siddharth Bansal <sidd190bansal@gmail.com>
2025-07-14 12:35:48 +0530

    Setup bpt Codes

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+
+const app = express();
+
+app.post('/signup',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    //db-call
+})
+
+app.post('/signin', (req, res) => {
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    },JWT_SECRET);
+
+    res.json({
+        token
+    });
+})
+
+app.post('/room',middleware,(req, res) => {
+    //db-call
+})
+
+app.listen(5000, () => {
+  console.log("HTTP backend is running on port 3000");
+});
---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,29 @@
+import {WebSocketServer} from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws,request) {
+
+    const url = request.url;
+    if(!url){
+        return;
+    }
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token')?? "";
+    const decoded = jwt.verify(token,JWT_SECRET)
+
+    if(!decoded || !(decoded as JwtPayloadg).userId){
+        ws.close();
+        return;
+    }
+
+    ws.on('error',console.error);
+
+    wss.on('message', function message(data) {
+        console.log('received: %s', data);
+        // Echo the received message back to the client
+        ws.send(`Echo: ${data}`);
+    });
+})
